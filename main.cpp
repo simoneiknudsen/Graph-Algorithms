5dc7baa992839c82fbc32cc6039daa78b96bba12
simoneiknudsen <simoneknudsen29@gmail.com>
2017-05-06 22:19:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "graph.hpp"
+
 using namespace graph;
 using namespace tags;
 using namespace std;
@@ -6,7 +7,7 @@ using namespace std;
 int main(){
 	auto g = diGraph<Directed>();
 
-    //Testing Bellman Ford algorithm
+	//Testing Bellman Ford algorithm
 	auto v1 = addVertex(g,1);
 	auto v2 = addVertex(g,2);
 	auto v3 = addVertex(g,3);
